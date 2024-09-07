e2a4de9e8d3cb6e95082522eda7870c178532793
Alexander Sauro <112291051+alxbro1@users.noreply.github.com>
2024-09-07 14:00:45 -0300

    Delete src directory

--- src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { Slice } from "./userReducer";
-import { appoinmentSlice } from "./appoinmentReducer";
-
-export const store = configureStore({
-  reducer: { user: Slice.reducer, appoinments: appoinmentSlice.reducer },
-});
--- src/views/Home.jsx
+++
@@ -1,9 +0,0 @@
-import { FirstPHome } from "../components/FirstPHome";
-
-export const Home = () => {
-  return (
-    <>
-      <FirstPHome/>
-    </>
-  );
-};
